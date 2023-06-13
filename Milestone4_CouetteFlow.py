5efecbc4842002c313214a5465b70cdc35ce7a75
Horus26 <51298668+Horus26@users.noreply.github.com>
2023-06-13 07:15:41 +0200

    CouetteFlowVelocityStrengthOverTimePlots

--- Milestone4_CouetteFlow.py
+++ Milestone4_CouetteFlow.py
@@ -9,8 +9,8 @@ def couette_flow():
     """
     
     # initialize LBM
-    grid_size_x = 10
-    grid_size_y = 10
+    grid_size_x = 200
+    grid_size_y = 100
     omega = 0.6
 
     top_boundary_velocity = 0.1
@@ -27,77 +27,113 @@ def couette_flow():
     timesteps = 5000
     simulated_velocity_field_tCyx = []
     simulated_velocity_field_tCyx.append(lbm.velocity_field_Cyx)
+    print("Simulating Couette flow...")
     for i in range(timesteps):
         lbm.step()
         lbm.update_velocity_field()
         simulated_velocity_field_tCyx.append(lbm.velocity_field_Cyx)
 
-    # plot velocity field streamplot
+    # # plot velocity field streamplot
     fig_velocity_over_time = plt.figure()
     ax1 = fig_velocity_over_time.add_subplot(111)
-    ax1.set_title("Velocity field over time")
+    # ax1.set_title("Velocity field over time")
     ax1.set_xlabel("x")
     ax1.set_ylabel("y")
     ax1.set_aspect("equal")
-    ax1.set_xlim(0, lbm.width)
-    # ax1.set_ylim(lbm.height, 0)
+    ax1.set_xlim(-10, lbm.width+10)
+    ax1.set_ylim(-10, lbm.height+10)
     x = np.arange(lbm.width)
     y = np.arange(lbm.height)
     X, Y = np.meshgrid(x,y)
 
     analytical_solution_velocity_x = top_boundary_velocity * np.arange(lbm.height-2, 0, -1) / (lbm.height-2)
-    print(analytical_solution_velocity_x)
     simulated_velocity_x = np.array(simulated_velocity_field_tCyx)[-1, 0, :, :]
-    print(simulated_velocity_x[1:-1, 4])
-    # plot velocity profile 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.plot(analytical_solution_velocity_x / top_boundary_velocity, np.arange(lbm.height-2, 0, -1) / (lbm.height -2), label="Analytical solution")
-    # ax.plot(simulated_velocity_x[1:-1, 4] / top_boundary_velocity, np.arange(lbm.height-2, 0, -1) / (lbm.height -2), label="Simulated solution")
-    # ax.set_xlabel("Velocity / U_x")
-    # ax.set_ylabel("y/H")
-    # ax.set_title("Velocity profile")
-    # ax.legend()
+    max_velocity = np.max(np.array(simulated_velocity_field_tCyx)[:, 0, :, :])
+    safe_timesteps = [1, 10, 100, 1000, timesteps-1]
 
-    # for i in range(timesteps):        
-    #     # plot strength of velocity field with imshow
-    #     if i%99 == 0:
-    #         ax1.set_title("Velocity field over time, t = " + str(i))
-    #         velocity_magnitude_field_yx = np.sqrt(simulated_velocity_field_tCyx[i][0]**2 + simulated_velocity_field_tCyx[i][1]**2)
-    #         mappable = ax1.imshow(velocity_magnitude_field_yx, cmap="hot", origin="upper")
-    #         cbar = fig_velocity_over_time.colorbar(mappable, ax=ax1, extend='both')
-    #         cbar.minorticks_on()
-    #         cbar.set_label("Velocity strength")
-    #         plt.tight_layout()
-    #         plt.pause(0.01)
-    #         cbar.remove()
+    # plot strength of velocity field
+    for i in range(timesteps):        
+        if i%50 == 0 or i in safe_timesteps:
+            ax1.set_xlabel("x")
+            ax1.set_ylabel("y")
+            ax1.set_aspect("equal")
+            ax1.set_xlim(-10, lbm.width+10)
+            ax1.set_ylim(-10, lbm.height+10)
+            # ax1.set_title("Velocity field over time, t = " + str(i))
+            velocity_magnitude_field_yx = np.sqrt(simulated_velocity_field_tCyx[i][0]**2 + simulated_velocity_field_tCyx[i][1]**2)
+            mappable = ax1.imshow(np.flip(velocity_magnitude_field_yx, axis=0), cmap="hot", origin="lower")
+            cbar = fig_velocity_over_time.colorbar(mappable, ax=ax1, extend='both')
+            cbar.set_ticks([0, max_velocity])
+            cbar.minorticks_on()
+            cbar.set_label("Velocity strength")
+            # draw the boundaries
+            ax1.plot([-0.5, lbm.width - 0.5], [lbm.height-1.5, lbm.height-1.5], color="red", label="Moving wall")
+            ax1.plot([-0.5, lbm.width - 0.5], [0.5, 0.5], color="black", label = "Fixed wall")
+            # draw vertical boundaries
+            ax1.plot([-0.5, -0.5], [0.5, lbm.height- 1.5], color="black", linestyle="dashed", label="Periodic boundaries")
+            ax1.plot([lbm.width-0.5, lbm.width-0.5], [0.5, lbm.height - 1.5], color="black", linestyle="dashed")
+            ax1.legend(bbox_to_anchor=(0, 1.02, 1, 0.2), loc="lower left",
+                mode="expand", borderaxespad=0, ncol=3)
+            plt.tight_layout()
+            if i in safe_timesteps:
+                plt.savefig("CouetteFlowResults/CouetteFlow_VelocityField_t" + str(i) + ".png")
+            plt.pause(0.01)
+            cbar.remove()
+            ax1.clear()
     
-    # plot the velocity vectors for last timestep
-    fig_vectors = plt.figure()
-    ax_vectors = fig_vectors.add_subplot(111)
-    # ax_vectors.set_title("Velocity field at t = " + str(timesteps))
-    ax_vectors.set_xlabel("x")
-    ax_vectors.set_ylabel("y")
-    ax_vectors.set_aspect("equal")
-    ax_vectors.set_xlim(-0.5, lbm.width)
-    ax_vectors.set_ylim(lbm.height, -0.5)
-    ax_vectors.quiver(X, Y, simulated_velocity_field_tCyx[-1][0], simulated_velocity_field_tCyx[-1][1])
-    # draw the boundaries
-    ax_vectors.plot([0.5, lbm.width - 1.5], [0.5, 0.5], color="red", label="Moving wall")
-    ax_vectors.plot([0.5, lbm.width - 1.5], [lbm.height-1.5, lbm.height-1.5], color="black", label = "Fixed wall")
-    # draw vertical boundaries
-    ax_vectors.plot([0.5, 0.5], [0.5, lbm.height- 1.5], color="black", linestyle="dashed", label="Periodic boundaries")
-    ax_vectors.plot([lbm.width-1.5, lbm.width-1.5], [0.5, lbm.height - 1.5], color="black", linestyle="dashed")
-    # draw all grid points
-    ax_vectors.scatter(X, Y, color="green", s=4, label="Grid points outside boundaries")
-    # make grid points outside the boundaries different color
-    ax_vectors.scatter(X[1:-1, 1:-1], Y[1:-1, 1:-1], color="blue", s=4, label="Grid points")
-    # do not show axes
-    ax_vectors.set_axis_off()
-    # plt.tight_layout()
-    ax_vectors.legend(bbox_to_anchor=(1.04, 1), borderaxespad=0)
+
+    def plot_velocity_vectors(timestep, lbm : LBM, ax_vectors, X, Y, simulated_velocity_field_tCyx, max_velocity):
+        ax_vectors.set_xlabel("x")
+        ax_vectors.set_ylabel("y")
+        ax_vectors.set_aspect("equal")
+        ax_vectors.set_title("Velocity field at t = " + str(timestep))
+        ax_vectors.set_xlim(-0.5, lbm.width)
+        ax_vectors.set_ylim(lbm.height, -0.5)
+        data_timestep = np.array(simulated_velocity_field_tCyx)[timestep, :, :, :]
+        ax_vectors.quiver(X, Y, data_timestep[0], data_timestep[1], scale=max_velocity*10)
+        # draw the boundaries
+        ax_vectors.plot([-0.5, lbm.width - 0.5], [0.5, 0.5], color="red", label="Moving wall")
+        ax_vectors.plot([-0.5, lbm.width - 0.5], [lbm.height-1.5, lbm.height-1.5], color="black", label = "Fixed wall")
+        # draw vertical boundaries
+        ax_vectors.plot([-0.5, -0.5], [0.5, lbm.height- 1.5], color="black", linestyle="dashed", label="Periodic boundaries")
+        ax_vectors.plot([lbm.width-0.5, lbm.width-0.5], [0.5, lbm.height - 1.5], color="black", linestyle="dashed")
+        # draw all grid points
+        ax_vectors.scatter(X, Y, color="green", s=4, label="Grid points outside boundaries")
+        # make grid points outside the boundaries different color
+        ax_vectors.scatter(X[1:-1, :], Y[1:-1, :], color="blue", s=4, label="Grid points")
+        # do not show axes
+        # ax_vectors.set_axis_off()
+        # plt.tight_layout()
+        # ax_vectors.legend(bbox_to_anchor=(1.04, 1), borderaxespad=0)
+    # plt.tight_layout(rect=[0, 0, 0.75, 1])
+    # plt.show()
+
+    # plot velocity profile four times over all timesteps
+    fig_velocity_profile = plt.figure()
+    
+    for i in range(0, timesteps, int(timesteps/4)):
+        ax_vel_profile = fig_velocity_profile.add_subplot(2, 2, int(i/(timesteps/4))+1)
+        plot_velocity_vectors(i, lbm, ax_vel_profile, X, Y, simulated_velocity_field_tCyx, max_velocity)
     plt.tight_layout(rect=[0, 0, 0.75, 1])
     plt.show()
 
+    # # plot velocity for all y values at x = 1 at 4 timesteps as one horizontal bar plot
+    # fig_velocity_profile = plt.figure()
+    # ax_vel_profile = fig_velocity_profile.add_subplot(111)
+    # ax_vel_profile.set_xlabel("Velocity")
+    # ax_vel_profile.set_ylabel("y")
+    # ax_vel_profile.set_ylim(0, lbm.height-1)
+    # # ax_vel_profile.set_xlim(0, max_velocity)
+    # # ax_vel_profile.set_aspect("equal")
+    # y = np.arange(lbm.height-1, -1, -1)
+    # colors = matplotlib.cm.rainbow(np.linspace(0, 1, 4))
+    # for i in range(timesteps-1, 0, -int(timesteps/4)):
+    #     ax_vel_profile.barh(y, np.array(simulated_velocity_field_tCyx)[i, 0, :, 1], height=-1, align="edge", label="t = " + str(i), color=colors[int(i/(timesteps/4))])
+
+    # ax_vel_profile.legend()
+    # # plt.tight_layout(rect=[0, 0, 0.75, 1])
+    # plt.show()
+
+    plt.show()
 if __name__ == "__main__":
     couette_flow()
